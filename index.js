f4977a1da64f601de3713fe26ebe7211ad3a174d
Edu Lazaro <edu@edulazaro.com>
2020-05-25 00:17:24 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Requerimos paquete terminal-kit
+const term = require( 'terminal-kit' ).terminal;
+
+// Asignamos el texto a una variable
+const texto = "Hola desde Node!";
+
+// Mostramos el texto
+term.magenta(texto) ;
